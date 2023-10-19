102775f39378f09515d0e52dc000871c6fa097e2
ArthurAndrad3 <arthur-amf@homail.com>
2023-10-19 15:46:33 -0300

    aa

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,23 @@ import Box from '@mui/material/Box';
 const Footer = () => {
   return (
     <Box>
-      <h1>oi</h1>
+      <img
+        src="../../../public/assets/Logo2.png"
+        alt="Logo2"
+        style={{
+          // display: { xs: 'flex', md: 'none' },
+
+          marginLeft: '0.5%', // Ajuste o espaçamento direito conforme necessário
+          flexGrow: 1,
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          letterSpacing: '.3rem',
+          color: 'inherit',
+          textDecoration: 'none',
+          height: 'auto', // Ajuste a altura conforme necessário
+          maxWidth: '150px', // Garante que a imagem não exceda a largura máxima
+        }}
+      />
     </Box>
   );
 };
