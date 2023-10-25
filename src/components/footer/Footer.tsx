f720481909c2ffe82a3e09da56d96dbdcc6d538b
ArthurAndrad3 <arthur-amf@homail.com>
2023-10-25 10:24:19 -0300

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,15 @@
 import Box from '@mui/material/Box';
-
+import Toolbar from '@mui/material/Toolbar';
 const Footer = () => {
   return (
-    <Box>
+    <Toolbar sx={{ height: '15rem', display: 'flex', alignItems: 'center' }}>
       <img
         src="../../../public/assets/Logo2.png"
         alt="Logo2"
         style={{
           // display: { xs: 'flex', md: 'none' },
 
-          marginLeft: '0.5%', // Ajuste o espaçamento direito conforme necessário
+          marginLeft: '7%', // Ajuste o espaçamento direito conforme necessário
           flexGrow: 1,
           fontFamily: 'monospace',
           fontWeight: 700,
@@ -17,10 +17,30 @@ const Footer = () => {
           color: 'inherit',
           textDecoration: 'none',
           height: 'auto', // Ajuste a altura conforme necessário
-          maxWidth: '150px', // Garante que a imagem não exceda a largura máxima
+          maxWidth: '250px', // Garante que a imagem não exceda a largura máxima
         }}
       />
-    </Box>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: { xs: 'none', md: 'flex' },
+          justifyContent: 'center',
+          marginLeft: '20pm',
+        }}
+      >
+        <h1>oi</h1>
+      </Box>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: { xs: 'none', md: 'flex' },
+          justifyContent: 'center',
+          marginLeft: '20pm',
+        }}
+      >
+        <h1>oi</h1>
+      </Box>
+    </Toolbar>
   );
 };
 
