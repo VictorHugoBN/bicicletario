a66d09753bada19956596ae195463087e1da4131
ArthurAndrad3 <arthur-amf@homail.com>
2023-11-14 20:44:55 -0300

    a

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -17,12 +17,13 @@ const Footer = () => {
         display: 'flex',
         alignItems: 'center',
         marginBottom: '-2rem',
+        marginTop: '4rem',
         backgroundImage:
-          'linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(0, 0, 255, 0.7), rgba(0, 0, 128, 0.8))',
+          'linear-gradient(to bottom,rgba(255, 255, 255, 0.8) 0%, rgba(205, 135, 2, 0.9) 20%, rgba(205, 106, 0, 0.9) 100%)',
       }}
     >
       <img
-        src="../../../public/assets/Logo2.png"
+        src="../../../public/assets/Logo.png"
         alt="Logo2"
         onClick={() => navigate(`/`)}
         style={{
@@ -48,6 +49,7 @@ const Footer = () => {
               display: 'flex',
               flexDirection: 'column',
               marginLeft: '5rem',
+              color: 'white',
               ':hover': {
                 backgroundColor: 'transparent',
                 color: '#CD6A00',
@@ -66,7 +68,12 @@ const Footer = () => {
         }}
       >
         <Typography
-          sx={{ fontSize: '1rem', lineHeight: '1rem', marginRight: '1.5rem' }}
+          sx={{
+            fontSize: '1rem',
+            lineHeight: '1rem',
+            marginRight: '1.5rem',
+            color: 'white',
+          }}
         >
           Onde estamos: Lorem ipsum dolor, sit amet consectetur adipisicing
           elit. Unde minus excepturi quis delectus, voluptatibus neque
@@ -74,14 +81,19 @@ const Footer = () => {
           est atque ratione eos nam?
         </Typography>
         <Typography
-          sx={{ fontSize: '1rem', lineHeight: '1rem', marginTop: '1rem' }}
+          sx={{
+            fontSize: '1rem',
+            lineHeight: '1rem',
+            marginTop: '1rem',
+            color: 'white',
+          }}
         >
           Contato: (99)99999-9999
         </Typography>
         <Box sx={{ display: 'flex', gap: '0.5rem', marginTop: '1rem' }}>
-          <InstagramIcon />
-          <FacebookIcon />
-          <LinkedInIcon />
+          <InstagramIcon sx={{ color: 'white' }} />
+          <FacebookIcon sx={{ color: 'white' }} />
+          <LinkedInIcon sx={{ color: 'white' }} />
           <XIcon />
         </Box>
       </Box>
