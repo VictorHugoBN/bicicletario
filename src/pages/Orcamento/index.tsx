386f005c14600920ca1e010b532fd66ece7b35ee
ArthurAndrad3 <arthur-amf@homail.com>
2023-11-21 00:28:59 -0300

    opa

--- src/pages/Orcamento/index.tsx
+++ src/pages/Orcamento/index.tsx
@@ -14,144 +14,144 @@ const Orcamento = () => {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+        margin: 0,
       }}
     >
-      <Container maxWidth={false}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column-reverse', md: 'row' },
+          alignItems: { xs: 'center', md: 'stretch' },
+          margin: 0,
+        }}
+      >
         <Box
           sx={{
+            flex: 1,
+            backgroundColor: 'secondary.main',
             display: 'flex',
-            flexDirection: { xs: 'column-reverse', md: 'row' },
-            alignItems: { xs: 'center', md: 'stretch' },
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            gap: '2rem',
+            width: '100%',
           }}
         >
+          <Typography
+            align="center"
+            sx={{
+              color: theme.palette.primary.main,
+              fontWeight: 700,
+              fontSize: '2.5rem',
+            }}
+          >
+            Faça seu orçamento!
+          </Typography>
           <Box
             sx={{
-              flex: 1,
-              backgroundColor: 'secondary.main',
               display: 'flex',
+              gap: '1rem',
+              [theme.breakpoints.down('sm')]: {
+                flexDirection: 'column',
+              },
+            }}
+          >
+            <Button
+              startIcon={<TodayIcon sx={{ width: '2rem', height: '2rem' }} />}
+              sx={{
+                fontFamily: 'Amaranth',
+                fontSize: '1.5rem',
+                fontWeight: 600,
+              }}
+            >
+              Orcamento
+            </Button>
+            <Button
+              startIcon={<PhoneIcon sx={{ width: '2rem', height: '2rem' }} />}
+              sx={{
+                fontFamily: 'Amaranth',
+                fontSize: '1.5rem',
+                fontWeight: 600,
+              }}
+            >
+              Contato
+            </Button>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            flex: 1,
+            backgroundColor: 'secondary.main',
+            position: 'relative',
+          }}
+        >
+          <img
+            src={bicicletarioImage}
+            alt="oooo"
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+          <Box
+            sx={{
+              display: { xs: 'none', md: 'flex' },
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
               alignItems: 'center',
               justifyContent: 'center',
               flexDirection: 'column',
               gap: '2rem',
-              paddingY: '4rem',
+              width: '50%',
+              height: '50%',
+              [theme.breakpoints.down('sm')]: {
+                width: '100%',
+                height: '100%',
+              },
             }}
           >
-            <Typography
-              align="center"
-              sx={{
-                color: theme.palette.primary.main,
-                fontWeight: 700,
-                fontSize: '2.5rem',
-              }}
-            >
-              Faça seu orçamento!
-            </Typography>
             <Box
               sx={{
+                backgroundColor: 'rgba(0, 0, 0, 0.8)',
                 display: 'flex',
-                gap: '1rem',
-                [theme.breakpoints.down('sm')]: {
-                  flexDirection: 'column',
-                },
-              }}
-            >
-              <Button
-                startIcon={<TodayIcon sx={{ width: '2rem', height: '2rem' }} />}
-                sx={{
-                  fontFamily: 'Amaranth',
-                  fontSize: '1.5rem',
-                  fontWeight: 600,
-                }}
-              >
-                Orcamento
-              </Button>
-              <Button
-                startIcon={<PhoneIcon sx={{ width: '2rem', height: '2rem' }} />}
-                sx={{
-                  fontFamily: 'Amaranth',
-                  fontSize: '1.5rem',
-                  fontWeight: 600,
-                }}
-              >
-                Contato
-              </Button>
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              flex: 1,
-              backgroundColor: theme.palette.primary.main,
-              position: 'relative',
-            }}
-          >
-            <img
-              src={bicicletarioImage}
-              alt="oooo"
-              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-            />
-            <Box
-              sx={{
-                display: { xs: 'none', md: 'flex' },
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
                 alignItems: 'center',
                 justifyContent: 'center',
                 flexDirection: 'column',
-                gap: '2rem',
-                width: '50%',
-                height: '50%',
-                [theme.breakpoints.down('sm')]: {
-                  width: '100%',
-                  height: '100%',
-                },
+                padding: '2rem',
+                borderRadius: '1rem',
               }}
             >
-              <Box
+              <Typography
                 sx={{
-                  backgroundColor: 'rgba(0, 0, 0, 0.8)',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  flexDirection: 'column',
-                  padding: '2rem',
-                  borderRadius: '1rem',
+                  color: 'white',
+                  fontWeight: 700,
+                  fontSize: '2.5rem',
+                  textAlign: 'center',
+                  lineHeight: '3rem',
                 }}
               >
-                <Typography
-                  sx={{
-                    color: 'white',
-                    fontWeight: 700,
-                    fontSize: '2.5rem',
-                    textAlign: 'center',
-                    lineHeight: '3rem',
-                  }}
-                >
-                  Acesse a galeria!
-                </Typography>
-                <Button
-                  variant="contained"
-                  startIcon={
-                    <PhotoLibraryIcon sx={{ width: '2rem', height: '2rem' }} />
-                  }
-                  sx={{
-                    fontFamily: 'Amaranth',
-                    fontSize: '1.5rem',
-                    fontWeight: 600,
-                    paddingX: '1.5rem',
-                    paddingY: '0.5rem',
-                    borderRadius: '1rem',
-                    marginTop: '2rem',
-                  }}
-                >
-                  Galeria
-                </Button>
-              </Box>
+                Acesse a galeria!
+              </Typography>
+              <Button
+                variant="contained"
+                startIcon={
+                  <PhotoLibraryIcon sx={{ width: '2rem', height: '2rem' }} />
+                }
+                sx={{
+                  fontFamily: 'Amaranth',
+                  fontSize: '1.5rem',
+                  fontWeight: 600,
+                  paddingX: '1.5rem',
+                  paddingY: '0.5rem',
+                  borderRadius: '1rem',
+                  marginTop: '2rem',
+                }}
+              >
+                Galeria
+              </Button>
             </Box>
           </Box>
         </Box>
-      </Container>
+      </Box>
     </Box>
   );
 };
