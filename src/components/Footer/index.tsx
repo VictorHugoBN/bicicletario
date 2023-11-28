4f9daf03a3603bcdd443a70c367cde5b7915c5cb
ArthurAndrad3 <arthur-amf@homail.com>
2023-11-28 18:40:36 -0300

    footer changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,10 @@
 import { Box, Theme, Toolbar, Typography } from '@mui/material';
 import FooterLogo from '../..//assets/Bicicletário fácil Logo Principal Transparente.svg';
 import { makeStyles } from '@mui/styles';
+import { Routes } from '../../utils/enums/Routes';
+import { useNavigate } from 'react-router-dom';
+import { theme } from '../../styles/theme';
+import Instagram from '@mui/icons-material/Instagram';
 
 const useStyles = makeStyles((theme: Theme) => ({
   footerToolbar: {
@@ -21,18 +25,87 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 const Footer = () => {
+  const navigate = useNavigate();
   const classes = useStyles();
   return (
     <Box>
       <Toolbar className={classes.footerToolbar}>
         <img src={FooterLogo} alt="Logo" className={classes.footerLogo} />
+        <Box
+          sx={{
+            [theme.breakpoints.down('lg')]: {
+              display: 'none',
+            },
+          }}
+        >
+          <Box>
+            {Routes.values.map((page) => (
+              <Typography key={page} onClick={() => navigate(`/${page}`)}>
+                <Typography
+                  sx={{
+                    cursor: 'pointer',
+                    '&:hover': {
+                      color: 'white',
+                    },
+                  }}
+                >
+                  {Routes.getLabel(page)}
+                </Typography>
+              </Typography>
+            ))}
+          </Box>
+        </Box>
         <Box>
-          <Box>
-            <Typography>Lorem ipsum dolor, sit. similique.</Typography>
-          </Box>
-          <Box>
-            <Typography>Lorem ipsum dolor, sit. similique.</Typography>
-          </Box>
+          <Typography
+            sx={{
+              [theme.breakpoints.down('lg')]: {
+                fontSize: '0.8rem',
+              },
+            }}
+          >
+            Nobrutec Comercio e Servicos de Serralheria e Equipamentos
+            Eletronicos Eireli
+          </Typography>
+          <Typography
+            sx={{
+              [theme.breakpoints.down('lg')]: {
+                fontSize: '0.8rem',
+              },
+            }}
+          >
+            15.082.870/0001-24
+          </Typography>
+          <Typography
+            sx={{
+              [theme.breakpoints.down('lg')]: {
+                fontSize: '0.8rem',
+              },
+            }}
+          >
+            brunobicicletario@gmail.com
+          </Typography>
+          <Typography
+            sx={{
+              [theme.breakpoints.down('lg')]: {
+                fontSize: '0.8rem',
+              },
+            }}
+          >
+            +55 21 98510-0734
+          </Typography>
+          <Instagram
+            sx={{
+              cursor: 'pointer',
+              '&:hover': {
+                color: 'white',
+              },
+            }}
+            onClick={() => {
+              window.open(
+                `https://instagram.com/bicicletariofacil?igshid=OGQ5ZDc2ODk2ZA==`,
+              );
+            }}
+          ></Instagram>
         </Box>
       </Toolbar>
     </Box>
