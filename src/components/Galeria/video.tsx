d0f0ce9ec4365678fc6062d0e6fc8358bb39413c
ArthurAndrad3 <arthur-amf@homail.com>
2023-12-13 20:59:22 -0300

    video galeria

---
+++ src/components/Galeria/video.tsx
@@ -0,0 +1,44 @@
+import { Box } from '@mui/material';
+import video from '../../assets/bike_-_82636 (Original).mp4';
+import { useState, useRef } from 'react';
+
+const Video = () => {
+  const [isPlaying, setIsPlaying] = useState(true);
+  const videoRef = useRef(null);
+
+  const handleVideoClick = () => {
+    const videoElement = videoRef.current;
+
+    if (videoElement) {
+      if (isPlaying) {
+        videoElement.pause();
+      } else {
+        videoElement.play();
+      }
+
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  return (
+    <>
+      <Box>
+        <video
+          ref={videoRef}
+          autoPlay
+          loop
+          muted
+          width="100%"
+          height="600px"
+          style={{ objectFit: 'cover', cursor: 'pointer' }}
+          onClick={handleVideoClick}
+        >
+          <source src={video} type="video/mp4" />
+          Seu navegador não suporta o elemento de vídeo.
+        </video>
+      </Box>
+    </>
+  );
+};
+
+export default Video;
--- src/pages/Galeria/index.tsx
+++ src/pages/Galeria/index.tsx
@@ -1,19 +1,14 @@
-import { Paper, Typography } from '@mui/material';
+import { Box } from '@mui/material';
+
+import Video from '../../components/Galeria/video';
 
 const Galeria = () => {
   return (
-    <Paper
-      elevation={3}
-      style={{ padding: '20px', maxWidth: '600px', margin: 'auto' }}
-    >
-      <Typography variant="h5" gutterBottom>
-        Meu Vídeo
-      </Typography>
-      <video controls width="100%" height="auto">
-        <source src="caminho/do/seu/video.mp4" type="video/mp4" />
-        Seu navegador não suporta o elemento de vídeo.
-      </video>
-    </Paper>
+    <>
+      <Video />
+
+      <Box sx={{ height: '30rem' }}></Box>
+    </>
   );
 };
 
