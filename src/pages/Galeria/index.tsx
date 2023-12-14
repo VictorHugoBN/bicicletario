039d606f7ef54b955d4834c8f3c22afde6b22a0e
ArthurAndrad3 <arthur-amf@homail.com>
2023-12-14 11:21:54 -0300

    animação galeria

--- src/pages/Galeria/index.tsx
+++ src/pages/Galeria/index.tsx
@@ -5,8 +5,15 @@ import ImagemSlide from '../../components/Galeria/imagemSlide';
 import imagem1 from '../../assets/bicicletarioHome.jpg';
 import imagem2 from '../../assets/Bicicletário fácil Logo Preto Fundo.svg';
 import imagem3 from '../../assets/bicicletarioHome.jpg';
+import { motion } from 'framer-motion';
+import useScrollAnimation from '../../hooks/useScrollAnimation';
 
 const Galeria = () => {
+  const variants = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
+  const animation1 = useScrollAnimation();
   const images = [imagem1, imagem2, imagem3, imagem1];
 
   // Repetir as imagens para criar um efeito de loop
@@ -24,8 +31,16 @@ const Galeria = () => {
         backgroundColor: theme.palette.background.default,
       }}
     >
-      <Video />
-      <ImagemSlide imagens={repeatedImages} />
+      <motion.div
+        variants={variants}
+        initial="hidden"
+        animate={animation1.controls}
+        ref={animation1.ref}
+      >
+        <Video />
+
+        <ImagemSlide imagens={repeatedImages} />
+      </motion.div>
     </Box>
   );
 };
